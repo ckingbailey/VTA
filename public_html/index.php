2f663b46cc29a4856132256a713a440ab1634f4d
ckb <ckingbailey@gmail.com>
2018-07-23 16:43:56 -0700

    under construction landing page replaces old landing page

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,26 @@
 <?php
-  session_start();
-  $title = 'SVBX - Home';
-  include('filestart.php');
-?>
-  <header class="page-header landing-page grey-bg">
-      <div class="container main-content jumbotron">
-        <img src="/assets/img/vta_logo.png">
-        <h1>Silicon Valley Berryessa Extension</h1>
-        <p>This site is for use by personnel working on the SVBX project. If you are not working upon the SVBX project, but would like some information, please click on the 'Learn More' button below.</p>
-        <p><a class="btn btn-primary btn-lg" href="http://www.vta.org/News-and-Media/Connect-with-VTA/Phase-I-of-BART-Silicon-Valley-Update#.WqbH0WrwZaQ" role="button">Learn more &raquo;</a></p>
-      </div>
-  </header>
+  $base = $_SERVER['DOCUMENT_ROOT'] . '/..';
+  require "$base/vendor/autoload.php";
 
-<?php include('fileend.php'); ?>
+  session_start();
+  
+  // instantiate Twig
+  $loader = new Twig_Loader_Filesystem("$base/templates");
+  $twig = new Twig_Environment($loader,
+      [
+          'debug' => true
+      ]
+  );
+  $twig->addExtension(new Twig_Extension_Debug());
+  
+  // base context
+  $context = [
+      'title' => 'Under Construction - Come back soon',
+      'navbarHeading' => null,
+      'navHeadingLink' => '/',
+      'navItems' => [],
+      'pageHeading' => '🚧 Under Construction 🚧'
+  ];
+  
+  $twig->display('page.html', $context);
+?>
